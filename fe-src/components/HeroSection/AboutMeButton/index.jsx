478f934b242ce42cb95d27d2f6159ec285828204
william-wibowo <williamciptono@gmail.com>
2024-08-11 21:57:40 +0700

    feat: add about me page, might consider to change this later

---
+++ fe-src/components/HeroSection/AboutMeButton/index.jsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import Link from 'next/link';
+import { ArrowUpRightIcon } from '@heroicons/react/16/solid';
+
+const AboutMeButton = () => {
+
+    return (
+        <Link className="rounded-full bg-clip-padding bg-gradient-to-r from-secondary-400 to-primary-600 hover:from-primary-800 text-white text-l my-4 py-2 px-4" href="/about" scroll={false}>
+            <div className='inline-flex group justify-center'>
+                <div>About Me</div>
+                <ArrowUpRightIcon className='fill-white h-6'/> 
+            </div>
+        </Link>
+    );
+}
+
+export default AboutMeButton;
--- fe-src/components/HeroSection/HeroTitle/index.tsx
+++ fe-src/components/HeroSection/HeroTitle/index.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
-import Link from "next/link";
+import AboutMeButton from '../AboutMeButton';
 
 const HeroTitle = () => {
   return (
@@ -34,22 +34,7 @@ const HeroTitle = () => {
         <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
         Full Stack Data & Platform Engineer from Indonesia, experienced in both developing & maintaining data platforms and ETL-ELT pipelines. Look forward to collaborate with you!
         </p>
-        {/* <div>
-        <Link
-            href="/#contact"
-            className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
-        >
-            Contact Me
-        </Link>
-        <Link
-            href="/"
-            className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-        >
-            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-            Download CV
-            </span>
-        </Link>
-        </div>  */}
+        <AboutMeButton/>
     </motion.div>
   );
 };
