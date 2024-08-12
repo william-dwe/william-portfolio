c3bfc648ddf92143da3616093aaaa21de60627e1
william-wibowo <williamciptono@gmail.com>
2024-08-12 10:26:11 +0700

    feat: add transisiton color on about me button

--- fe-src/components/HeroSection/AboutMeButton/index.jsx
+++ fe-src/components/HeroSection/AboutMeButton/index.jsx
@@ -6,7 +6,7 @@ import { ArrowUpRightIcon } from '@heroicons/react/16/solid';
 const AboutMeButton = () => {
 
     return (
-        <Link className="rounded-full bg-clip-padding bg-gradient-to-r from-secondary-400 to-primary-600 hover:from-primary-800 text-white text-l my-4 py-2 px-4" href="/about" scroll={false}>
+        <Link className="rounded-full bg-clip-padding transition-colors bg-gradient-to-r from-secondary-400 to-primary-600 hover:from-primary-800 text-white text-l my-4 py-2 px-4" href="/about" scroll={false}>
             <div className='inline-flex group justify-center'>
                 <div>About Me</div>
                 <ArrowUpRightIcon className='fill-white h-6'/> 
