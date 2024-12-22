55be4efb8ed5315e7043c13996fc7b866f56416f
william-wibowo <williamciptono@gmail.com>
2024-12-22 16:30:37 +0700

    feat: init project page

--- fe-src/app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Project() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      your project here
-    </main>
-  );
-}
